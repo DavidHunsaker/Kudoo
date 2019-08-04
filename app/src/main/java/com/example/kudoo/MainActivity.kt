c1876e3ff7ef93ba89a86a55b2aef8a1f37cc4ff
David Hunsaker <davidhunsaker@u.boisestate.edu>
2019-08-04 11:29:13 -0600

    Use database from MainActivity

--- app/src/main/java/com/example/kudoo/MainActivity.kt
+++ app/src/main/java/com/example/kudoo/MainActivity.kt
@@ -2,33 +2,47 @@ package com.example.kudoo
 
 import android.os.Bundle
 import android.support.design.widget.Snackbar
-import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.DefaultItemAnimator
 import android.support.v7.widget.DividerItemDecoration
 import android.support.v7.widget.LinearLayoutManager
+import com.example.kudoo.db.AppDatabase
+import com.example.kudoo.db.DB
 import com.example.kudoo.view.main.RecyclerListAdapter
-import com.example.kudoo.model.TodoItem
 
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
+import kotlinx.coroutines.*
+import kotlin.coroutines.CoroutineContext
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), CoroutineScope {
+
+    private lateinit var db: AppDatabase  // Stores an AppDatabase object
+
+    val uiScope = CoroutineScope(coroutineContext + SupervisorJob())
+
+    override val coroutineContext: CoroutineContext
+        get() = Dispatchers.Main
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
+        db = AppDatabase.getDatabse(applicationContext)
+        setUpRecyclerView()
 
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
         }
 
-        setUpRecyclerView()
     }
 
     private fun setUpRecyclerView() = with(recyclerViewTodos) {
-        adapter = RecyclerListAdapter(sampleData())  // Populates adapter/list with data
+        uiScope.launch {
+            val todos = sampleData().toMutableList()
+            adapter = RecyclerListAdapter(todos)  // Populates adapter/list with data
+        }
         layoutManager = LinearLayoutManager(this@MainActivity)
         itemAnimator = DefaultItemAnimator()
         addItemDecoration(
@@ -36,9 +50,6 @@ class MainActivity : AppCompatActivity() {
         )
     }
 
-    private fun sampleData() = mutableListOf(
-        TodoItem("Implement RecyclerView"),
-        TodoItem("Store to-dos in database"),
-        TodoItem("Delete to-dos on click")
-    )
+    private suspend fun sampleData() =
+        withContext(DB) { db.todoItemDao().loadAllTodos() }
 }
--- app/src/main/java/com/example/kudoo/db/AppDatabase.kt
+++ app/src/main/java/com/example/kudoo/db/AppDatabase.kt
@@ -3,6 +3,15 @@ package com.example.kudoo.db
 import android.arch.persistence.room.*
 import android.content.Context  // Needs access to Android context to build DB object
 import com.example.kudoo.model.TodoItem
+import android.arch.persistence.db.SupportSQLiteDatabase
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.asCoroutineDispatcher
+import kotlinx.coroutines.launch
+import java.util.concurrent.Executors
+
+val DB = Executors.newSingleThreadExecutor().asCoroutineDispatcher()
+
+val dbScope = CoroutineScope(DB)
 
 @Database(entities = [TodoItem::class], version = 1)  // TodoItem is only DB entity
 abstract class AppDatabase : RoomDatabase() {
@@ -13,9 +22,29 @@ abstract class AppDatabase : RoomDatabase() {
         fun getDatabse(ctx: Context): AppDatabase {
             if (INSTANCE == null) {
                 INSTANCE = Room.databaseBuilder(ctx, AppDatabase::class.java, "AppDatabase")
+                    .addCallback(prepopulateCallback(ctx))
                     .build()
             }
             return INSTANCE!!
+        }
+
+        private fun prepopulateCallback(ctx: Context): Callback {
+            return object: Callback() {
+                override fun onCreate(db: SupportSQLiteDatabase) {
+                    super.onCreate(db)
+                    populateWithSampleData(ctx)
+                }
+            }
+        }
+
+        private fun populateWithSampleData(ctx: Context) {
+            dbScope.launch {
+                with(getDatabse(ctx).todoItemDao()) {
+                    insertTodo(TodoItem("Create entity"))
+                    insertTodo(TodoItem("Add a DAO for data access"))
+                    insertTodo(TodoItem("Inherit from RoomDatabase"))
+                }
+            }
         }
     }
 
